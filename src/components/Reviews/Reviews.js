741701e7fd5bce2a5349dd0a52b781f8b1569f4c
Faizul-Hassan-2015 <86608620+Faizul-Hassan-2015@users.noreply.github.com>
2022-04-04 15:00:50 +0600

    add reviews page information

--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import useReviews from '../../Hooks/Hooks';
+import Review from '../Review/Review';
+import './Reviews.css';
 
 const Reviews = () => {
+    const [reviews, setReviews] = useReviews()
     return (
         <div>
-            <h2>Reviews</h2>
+            <div>
+            <h2 className='review-h2'>All Customer Reviews:({reviews.length})</h2>
+            </div>
+            <div className='all-reviews'>
+                {
+                    reviews.map(review => <Review
+                    key={review.id}
+                    review={review}
+
+                    ></Review>)
+                }
+            </div>
         </div>
     );
 };
